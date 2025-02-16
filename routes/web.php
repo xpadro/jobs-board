63e45c61edddc3efa6afca89501c8f2147fb1b27
xpadro <xpadro.sw@gmail.com>
2025-02-16 18:00:54 +0100

    fix n+1 problem

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,13 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function ()  {
-    return view('jobs', ['jobs' => Job::all()]);
+    //Instead of Job::all() we eagerly fetch all employer records to avoid n+1 problem
+    //You can disable lazy loading at the entire application level in AppServiceProvider.php
+    //
+    //'employer' is the relationship name (the name of the function in Job.php)
+    $jobs = Job::with('employer')->get();
+
+    return view('jobs', ['jobs' => $jobs]);
 });
 
 Route::get('/jobs/{id}', function ($id)  {
