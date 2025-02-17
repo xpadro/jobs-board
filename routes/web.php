78f456ce86ed9ab37f8ec8f225ccdf41b73465c3
xpadro <xpadro.sw@gmail.com>
2025-02-17 16:26:16 +0100

    include pagination

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,25 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function ()  {
+Route::get('/jobs_no_pagination', function ()  {
     //Instead of Job::all() we eagerly fetch all employer records to avoid n+1 problem
     //You can disable lazy loading at the entire application level in AppServiceProvider.php
     //
     //'employer' is the relationship name (the name of the function in Job.php)
     $jobs = Job::with('employer')->get();
+
+    return view('jobs', ['jobs' => $jobs]);
+});
+
+Route::get('/jobs', function ()  {
+    //Pagination with page numbers
+    //$jobs = Job::with('employer')->paginate(10);
+
+    //Pagination with Previous and Next
+    //$jobs = Job::with('employer')->simplePaginate();
+
+    //Pagination with cursor
+    $jobs = Job::with('employer')->cursorPaginate();
 
     return view('jobs', ['jobs' => $jobs]);
 });
