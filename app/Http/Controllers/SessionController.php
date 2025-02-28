a83838104a06556f6d3ea3290ce136f3b10a535b
xpadro <xpadro.sw@gmail.com>
2025-02-28 18:31:34 +0100

    logout

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
@@ -12,5 +13,10 @@ class SessionController extends Controller
 
     public function store() {
         
+    }
+
+    public function destroy() {
+        Auth::logout();
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,4 @@ Route::post('/register', [RegisterUserController::class, 'store']);
 
 Route::get('/login', [SessionController::class, 'create']);
 Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
