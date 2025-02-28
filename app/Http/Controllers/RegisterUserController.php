465eb18a1f413c9893f08c66c96b6b78bb4ca0ef
xpadro <xpadro.sw@gmail.com>
2025-02-28 18:26:38 +0100

    register user

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterUserController extends Controller
 {
@@ -11,7 +14,18 @@ class RegisterUserController extends Controller
     }
 
     public function store() {
-        //return view('auth.register');
-        dd('TODO');
+        // 'confirmed' matches the 'field' with 'field_confirmation' by convention
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email', 'max:254'],
+            'password' => ['required', Password::min(5)->max(10), 'confirmed'] // password_confirmation field
+        ]);
+
+        $user = User::create($attributes);
+
+        Auth::login($user);
+
+        return redirect(('jobs'));
     }
 }
